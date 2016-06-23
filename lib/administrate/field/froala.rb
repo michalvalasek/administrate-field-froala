5724aa5466c88f4a2d8492091dcb963f06563c2b
Michal Valasek <michal.valasek@gmail.com>
2016-06-23 21:25:37 +0200

    BUGFIX: require missing dependency in the main class

--- lib/administrate/field/froala.rb
+++ lib/administrate/field/froala.rb
@@ -1,11 +1,12 @@
 require "administrate/field/text"
 require "rails"
 require "administrate/engine"
+require "wysiwyg-rails"
 
 module Administrate
   module Field
     class Froala < Administrate::Field::Text
-      VERSION = "0.1.0"
+      VERSION = "0.1.1"
 
       class Engine < ::Rails::Engine
         Administrate::Engine.add_javascript "administrate-field-froala/application"
